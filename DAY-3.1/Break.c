17841453760cd26eef8b2e3ae378493c9e7b637d
tymcs <tyasmin.mcs@gmail.com>
2024-06-18 10:46:34 +0600

    ok

---
+++ DAY-3.1/Break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+    int i;
+    for(i = 1 ; i <= 20 ; i++)
+    {
+        printf("%d\n",i);
+
+        continue;
+        if(i == 5)
+
+        if( i >= 15)
+        break;
+    }
+}
